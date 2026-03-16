96e90d4d6f6de8ffada41366f9d967ec801a7f39
Devashish Kumar <dk3127197@gmail.com>
2026-03-16 22:31:45 +0530

    Array methods in JS

--- arrays/array.js
+++ arrays/array.js
@@ -20,3 +20,32 @@ let name = "Devashish";
 console.log("Hello Everyone");
 console.log(name);
 console.log(`sum of a & b : ${a+b}`);
+
+// Methods of array:-
+// push() method in JS : add in end
+
+let cars = ["audi", "bmw", "maruti", "xuv"];  
+console.log(`Without changes: ${cars}`); //["audi", "bmw", "maruti", "xuv"]
+
+cars.push("toyota");
+console.log(`push in last : ${cars}`); //["audi", "bmw", "maruti", "xuv", "toyota"]
+
+// pop() method in JS : delete from end & returns it
+
+cars.pop();
+console.log(`delete from end : ${cars}`); //["audi", "bmw", "maruti", "xuv"]
+
+// unshift() method in JS : add to start
+cars.unshift("ferrari");
+console.log(`add to start : ${cars}`); // ["ferrari", "audi", "bmw", "maruti", "xuv"]
+
+cars.unshift("nano");
+console.log(`add nano to start : ${cars}`);  // ["nano", "ferrari", "audi", "bmw", "maruti", "xuv"]
+
+// shift() method in JS : delete from start & returns it
+cars.shift();
+console.log(`delete nano from start ${cars}`); //["ferrari", "audi", "bmw", "maruti", "xuv"]
+
+
+
+
