679e7915e350cd72b2d91cdecde3c825bd6243fd
Devashish Kumar <dk3127197@gmail.com>
2026-03-16 22:11:44 +0530

    JS basics

---
+++ basics/if.js
@@ -0,0 +1,45 @@
+// Arrays
+
+// let students = ["Sourav", "Sachin", "Anjani", "Tanya"]; // string array
+// let marks = [90, 91, 92, 93];  // number array
+
+// let student1 = ["sourav", 90, "physics", "nawada"]; // mixed array
+// let student2 = ["sachin", 91, "physics", "jamui"];
+// let student3 = ["anjani", 92, "physics", "nalanda"];
+// let student4 = ["tanya", 93, "arts", "madhepura"];
+
+// console.table(student1);
+// console.table(student2);
+// console.table(student3);
+// console.table(student4);
+
+let students = [
+  ["sourav", 90, "physics", "nawada"],
+  ["sachin", 91, "jamui"],
+  ["anjani", 92],
+  ["tanya", 93, "arts", "madhepura"],
+];
+
+console.table(students);
+console.log(students[0][0]);
+
+// students[0].length
+
+console.log(students[3][3].length);
+
+
+
+
+
+
+// let arr = [["sachin"], ["Raja"]]; //empty array
+// arr[0][1] = 36;
+// arr[0][2] = "chemistry";
+// arr[0][3] = "Barsoi";
+// arr[1][1] = 80;
+
+// console.table(arr);
+
+// // [[sachin], raja]
+
+
