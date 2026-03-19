55c76a22cbd3e69658a017fbbec0bd5a59931364
Devashish Kumar <dk3127197@gmail.com>
2026-03-19 22:26:07 +0530

    Added array methods in javascript

---
+++ arrays/array.method.js
@@ -0,0 +1,61 @@
+// // let followers = ["sourav", "mohit", "rohan", "sachin"];
+
+// // let blocked = followers.shift();
+
+// // console.log(blocked);
+// // console.log(followers);
+
+
+// // Methods of Arrays 
+// // 1. indexOf() method:-
+
+// let colors = ["yellow", "green", "blue"];
+// let answer = colors.indexOf("blue");// -1
+// console.log(answer);
+
+// // 2. includes() method:-
+
+// let name = ["rahul", "sohan", "satvik", "rachna"];
+// let result = name.includes("satvik");
+// console.log(result);
+
+// // 3. concat() method:-
+// let class2 = ["bhanu", "priyanka", "ritika", "ritu"];
+// let class1 = ["raja", "john", "rithik", "kartik"];
+
+
+// let mergedArray = class2.concat(class1);
+// console.log(mergedArray); // ["raja", "john", "rithik", "kartik", "bhanu", "priyanka", "ritika", "ritu"]
+
+
+// // 4. reverse() method:-
+// let arr = ["blue", "green", "yellow", "pink"];
+// let reverseArr = arr.reverse();
+// console.log(reverseArr); // ['pink', 'yellow', 'green', 'blue']
+
+// // 5. slice() method:-
+// let color = ["red", "yellow", "blue", "orange", "pink", "white"]; 
+// console.log(color.slice());// make a new array.
+// console.log(color);// old array is safe.
+
+// console.log(color.slice(2));// ['blue', 'orange', 'pink', 'white']
+
+// console.log(color.slice(3,5));//['orange', 'pink']
+
+// 5. splice() method:- (changes in original array)
+let newColor = ["red", "green", "blue", "yellow"];
+console.log(newColor.splice(0,2,"red","yellow"));// red, green
+console.log(newColor);
+
+// 6. sort() method:-
+let num = ["ball", "apple"];
+console.log(num.sort());
+
+
+
+
+
+
+
+
+
