33046e590c92bc6663181f7762e79b0bc09440b9
Devashish Kumar <dk3127197@gmail.com>
2026-03-24 07:08:44 +0530

    loops in JS

---
+++ loops/InfiniteLoop.js
@@ -0,0 +1,11 @@
+// for(let i = 0;i>=0; i++){
+//     console.log("hello world");
+// }
+
+// for(let i=1;i<=5;i--){
+    // statement 
+// }
+
+// for(let i=0; ; i++){
+//     // statement
+// }
---
+++ loops/loop.js
@@ -0,0 +1,57 @@
+console.log("Loops in JS");
+
+
+// Loops ;- used to iterate(repeat) a piece of code 
+// for loop in JS
+
+// for(initialisation;condition;updation){
+//      piece of code /do something
+// }
+
+// Question :- WAP in JS to input a number from user and print 
+// the table of that number.
+
+// let n = parseInt(prompt("enter a num : "));
+
+// for(let i = 1;i<= 10; i++){
+//     // console.log(n * i);
+//     console.log(`${n} x ${i} = ${n*i}`);
+// }
+
+// Q1. Print all odd numbers (1 to 20).
+
+// for(let i=1;i<= n; i++){
+//     if(i % 2 == 1){
+//         console.log(i);
+//     }
+    
+// }
+
+// Q2. print all odd numbers (20 to 1).
+
+// for(let i=n; i>=1; i--){
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+// }
+
+// Q3. print all even numbers (1 to 50).
+
+// for(let i=1;i<= n; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+    
+// }
+
+// Q4. print all even numbers (5 to 1).
+
+// for(let i=n;i>= 1; i--){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     } 
+// }
+
+
+
+
