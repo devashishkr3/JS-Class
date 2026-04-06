b90de3e513b07cc87c128250d5400fbfeef2c667
Devashish Kumar <dk3127197@gmail.com>
2026-04-06 20:58:02 +0530

    Set timeout function in js

---
+++ functions/setTimeout.js
@@ -0,0 +1,10 @@
+console.log("Welcome to setTimeout function example!");
+
+// setTimeout is a built-in JavaScript function that allows 
+// you to execute a function after a specified delay (in milliseconds).
+
+setTimeout(() =>{
+    console.log("Hello Everyone!!");
+}, 4000);
+
+console.log("Endline");
