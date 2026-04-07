4eb165c07308602c2f35782ab10badb85db4ed9b
Devashish Kumar <dk3127197@gmail.com>
2026-04-07 20:49:42 +0530

    forEach method in array

---
+++ array methods/foreach.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3,4,5];
+
+let print = (a) => {
+    console.log(a);
+};
+
+// print(2);
+
+arr.forEach(print);
--- functions/setTimeout.js
+++ functions/setTimeout.js
@@ -1,10 +1,36 @@
-console.log("Welcome to setTimeout function example!");
+// console.log("Welcome to setTimeout function example!");
 
-// setTimeout is a built-in JavaScript function that allows 
-// you to execute a function after a specified delay (in milliseconds).
+// // setTimeout is a built-in JavaScript function that allows 
+// // you to execute a function after a specified delay (in milliseconds).
+
+// setTimeout(() =>{
+//     console.log("Hello Everyone!!");
+// }, 4000);
+
+// console.log("Endline");
+
+// // set Interval Function in JS:- 
+
+// let Id = setInterval(() =>{
+//     console.log("Set Interval");
+// }, 2000);
+
+// setTimeout(() =>{
+//     clearInterval(Id);
+//     console.log("Interval Cleared");
+// }, 12000);
+
+
+//Q1. WA arrow Function to return/ find square of a 
+// number and print.
+
+//Q2. write a function that prints "Hello world" 5 times at a 
+// interval of 2 sec each. 
+
+let text = "Loading...";
+console.log(text);
 
 setTimeout(() =>{
-    console.log("Hello Everyone!!");
-}, 4000);
-
-console.log("Endline");
+    text = "Data Fetched.";
+    console.log(text);
+}, 3000);
