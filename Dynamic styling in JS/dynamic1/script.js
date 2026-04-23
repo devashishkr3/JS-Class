2eeb2f59542ff2c34706bf65647e390f86a91ed2
Devashish Kumar <dk3127197@gmail.com>
2026-04-23 21:42:46 +0530

    manipulating attribute(get attribute, set attribute) in JS

---
+++ Dynamic styling in JS/dynamic1/script.js
@@ -0,0 +1,43 @@
+let node1 = document.getElementById('node1');
+
+node1.setAttribute('class', 'red');
+
+let para = document.getElementById('para');
+
+para.style.color = 'blue';
+para.style.backgroundColor = 'pink';
+para.innerText = "Happy Janmashtmi";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let node1 = document.getElementById('node1');
+
+// console.log(node1.getAttribute('id'));
+// node1.setAttribute('class', 'heading');
+// console.log(node1.getAttribute('class'));
+
+// node1.style.color = 'red';
+
+// console.log(node1.classList);
+
